2778ad17527ed1b29c3e4cbdd9e626a5261d3641
Kamara Deo <kamaradeo@Kamara-Deo.local>
2018-04-09 15:09:35 +0300

    [Feature #156466913] Automate deployment and running of the app

--- fabfile.py
+++ fabfile.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from __future__ import with_statement
 from fabric.api import local, settings, hide
 import fnmatch
@@ -91,3 +92,22 @@ def set_up(user):
         return ("Setup finished and App running")
     else:
         return ("System user doesnt exist")
+=======
+from fabric.api import local
+def InstallDB(user):
+    local(" psql " +user+" -c 'CREATE DATABASE mrm_d'")
+
+def Run_Migration():
+    local("python manage.py init")
+    local("python manage.py migrate")
+    local("python manage.py upgrade")
+
+def Run_App():
+    local("python app.py")
+
+def prepare_to_host(user):
+    InstallDB(user)
+    Run_Migration()
+    Run_App()
+
+>>>>>>> [Feature #156466913] Automate deployment and running of the app
