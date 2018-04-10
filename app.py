eab73e6150a3946df857a3f20a6ff26108ac136c
Joyce Namuli <joyce.namuli@andela.com>
2018-04-10 11:53:34 +0300

    Create a databaser Instance using SQLAlchemy

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+#local imports
 from config import config
 
-
+#initializing a db instance
+db = SQLAlchemy()
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
