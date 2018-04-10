32d3fc23a6bf9fb7255c5d14cdedee952d017d45
Joyce Namuli <joyce.namuli@andela.com>
2018-04-10 11:54:58 +0300

    Add Migrations command

--- manage.py
+++ manage.py
@@ -1,12 +1,16 @@
 import os
 
 from flask_script import Manager, Shell
+from flask_migrate import Migrate, MigrateCommand
 
-from app import create_app
+#local imports
+from app import create_app, db
 import config
+import models
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
 manager = Manager(app)
 
 def make_shell_context():
@@ -16,7 +20,7 @@ manager.add_command(
     "shell", Shell(
         make_context=make_shell_context))
 
-
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
