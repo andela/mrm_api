480c0280309749bf399d4ba81667c9cc0996f370
PatrickCmd <pwalukagga@gmail.com>
2018-04-13 22:28:20 +0300

    [Feature #156461691] Change mock mutation data to fixtures file

--- tests/test_create_room.py
+++ tests/test_create_room.py
@@ -6,36 +6,16 @@ mrm_api = dirname(dirname(abspath(__file__)))
 sys.path.insert(0, mrm_api)
 
 from tests.base import BaseTestCase
+from tests.fixtures import room_mutation_query, room_mutation_response
 from database import db_session
 
 
 class TestCreateRoom(BaseTestCase):
 
     def test_room_creation(self):
-        query = '''
-                mutation {
-                    createRoom(name: "Mbarara", type: "Meeting", capacity: 4, floorId: 1) {
-                        room {
-                            name
-                            type
-                            capacity
-                            floorId
-                        }
-                    }
-                }
-            '''
-        execute_query = self.client.execute(query, context_value={'session': db_session})
+        execute_query = self.client.execute(
+            room_mutation_query,
+            context_value={'session': db_session})
         
-        expected_responese = {
-            "data": {
-                "createRoom": {
-                    "room": {
-                        "name": "Mbarara",
-                        "type": "Meeting",
-                        "capacity": 4,
-                        "floorId": 1,
-                    }
-                }
-            }
-        }
+        expected_responese = room_mutation_response
         self.assertEqual(execute_query, expected_responese)
