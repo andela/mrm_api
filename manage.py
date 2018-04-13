92e5465d5f57997cce0d76b8492d089ab074b1b0
Joyce Namuli <joyce.namuli@andela.com>
2018-04-13 10:50:34 +0300

    [ft-156461261] Use Alembic tool to run migrations

--- manage.py
+++ manage.py
@@ -1,16 +1,16 @@
 import os
 
 from flask_script import Manager, Shell
-from flask_migrate import Migrate, MigrateCommand
+
 
 #local imports
-from app import create_app, db
+from app import create_app
 import config
-import models
+
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-migrate = Migrate(app, db)
+
 manager = Manager(app)
 
 def make_shell_context():
@@ -20,7 +20,6 @@ manager.add_command(
     "shell", Shell(
         make_context=make_shell_context))
 
-manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
