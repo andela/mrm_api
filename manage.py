54e6cea1e3653060c51fe0e4c2457cf74afe7f5c
Joyce Namuli <joyce.namuli@andela.com>
2018-04-13 10:50:34 +0300

    [ft-156461261] Use Alembic tool to run migrations

--- manage.py
+++ manage.py
@@ -1,20 +1,20 @@
 import os
 
 from flask_script import Manager, Shell
-from flask_migrate import Migrate, MigrateCommand
+
 
 from app import create_app
 
 
 app = create_app(os.getenv('APP_SETTINGS') or 'default')
 #local imports
-from app import create_app, db
+from app import create_app
 import config
-import models
+
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-migrate = Migrate(app, db)
+
 manager = Manager(app)
 
 def make_shell_context():
@@ -24,7 +24,6 @@ manager.add_command(
     "shell", Shell(
         make_context=make_shell_context))
 
-manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
     manager.run()
--- models.py
+++ models.py
@@ -3,13 +3,14 @@ from sqlalchemy.orm import backref, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 #local imports.
-from app import db
 
+Base = declarative_base()
 # test class to run migration. It is expected to change.
-class User(db.Model):
-    __tablename__ = 'department'
+class User(Base):
+    __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     name = Column(String)
+    role = Column(String)
 
 
 
