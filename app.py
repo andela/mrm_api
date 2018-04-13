684fa7b5e2e7bcbcf2026b14881de54429e58e9f
Joyce Namuli <joyce.namuli@andela.com>
2018-04-13 10:50:34 +0300

    [ft-156461261] Use Alembic tool to run migrations

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+import sqlalchemy
 
 #local imports
 from config import config
 
-#initializing a db instance
-db = SQLAlchemy()
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
