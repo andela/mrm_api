ef84b12924aab66018a97f8b655e3ae95d1ae8bc
PatrickCmd <pwalukagga@gmail.com>
2018-04-16 22:07:16 +0300

    [Feature #156461691] Modularise models in different packages

---
+++ api/floor/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import (
+    Column, String, Integer, ForeignKey, func, 
+    DateTime, create_engine)
+from sqlalchemy.orm import relationship
+
+from helpers.database import Base
+from utilities.utility import Utility
+from block.models import Block
+
+
+class Floor(Base, Utility):
+    __tablename__ = 'floors'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    block_id = Column(Integer, ForeignKey('blocks.id'))
+    room = relationship('Room')
