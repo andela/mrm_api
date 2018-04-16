2399652cf6e7f4c7b776c17984b6c4f71c40fa82
PatrickCmd <pwalukagga@gmail.com>
2018-04-16 21:16:12 +0300

    [Feature #156461691] Modularise models in different packages

---
+++ equipment/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import (
+    Column, String, Integer, ForeignKey, func, 
+    DateTime, create_engine)
+from sqlalchemy.orm import relationship
+
+from helpers.database import Base
+from utilities.utility import Utility
+from room.models import Room
+
+
+class Equipment(Base, Utility):
+    __tablename__ = 'equipments'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    room_id = Column(Integer, ForeignKey('rooms.id'))
