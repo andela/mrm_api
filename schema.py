934f7eb35fe06f499c82c4e192472e0f9525c775
PatrickCmd <pwalukagga@gmail.com>
2018-04-16 21:22:13 +0300

    [Feature #156461691] Refactor to have general schema

--- schema.py
+++ schema.py
@@ -1,50 +1,17 @@
 import graphene
 
-from graphene import relay, Schema
-from graphene_sqlalchemy import (SQLAlchemyObjectType, 
-                                 SQLAlchemyConnectionField)
-
-from models import (
-    Room as RoomModel, Equipment as EquipmentModel)
+import room.schema
 
 
-class Room(SQLAlchemyObjectType):
-    
-    class Meta:
-        model = RoomModel
-        interfaces = (relay.Node, )
+class Query(
+    room.schema.Query
+):
+    pass
 
 
-class Equipment(SQLAlchemyObjectType):
-    
-    class Meta:
-        model = EquipmentModel
-        interfaces = (relay.Node, )
+class Mutation(
+    room.schema.Mutation
+):
+    pass
 
-
-class CreateRoom(graphene.Mutation):
-    
-    class Arguments:
-        name = graphene.String()
-        type = graphene.String()
-        capacity = graphene.Int()
-        floor_id = graphene.Int()
-    room = graphene.Field(Room)
-
-    def mutate(self, info, **kwargs):
-        room  = RoomModel(**kwargs)
-        room.save()
-
-        return CreateRoom(room=room)
-
-
-class Query(graphene.ObjectType):
-    node = relay.Node.Field()
-    rooms = SQLAlchemyConnectionField(Room)
-    equipments = SQLAlchemyConnectionField(Equipment)
-
-
-class Mutations(graphene.ObjectType):
-    create_room = CreateRoom.Field()
-
-schema = Schema(query=Query, mutation=Mutations)
+schema = graphene.Schema(query=Query, mutation=Mutation)
