09bb1a7e974bcacfe0123af26202c5a3e30eed04
PatrickCmd <pwalukagga@gmail.com>
2018-04-24 16:22:54 +0300

    [Feature #156461691] Refactor the schema

--- api/room/schema.py
+++ api/room/schema.py
@@ -1,6 +1,5 @@
 import graphene
 
-from graphql import GraphQLError
 from graphene import relay, Schema
 from graphene_sqlalchemy import (SQLAlchemyObjectType, 
                                  SQLAlchemyConnectionField)
@@ -25,9 +24,6 @@ class CreateRoom(graphene.Mutation):
     room = graphene.Field(Room)
 
     def mutate(self, info, **kwargs):
-        # for field in kwargs:
-        #     if not kwargs.get(field):
-        #         raise GraphQLError(f"Room {field} is required field")
         room  = RoomModel(**kwargs)
         room.save()
 
