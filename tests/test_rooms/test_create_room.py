a2c9f0aa491f64c2b3570401e2cc67bed38db6c3
PatrickCmd <pwalukagga@gmail.com>
2018-04-24 16:31:05 +0300

    [Feature #156461691] Add doc string to the test method

--- tests/test_rooms/test_create_room.py
+++ tests/test_rooms/test_create_room.py
@@ -16,6 +16,9 @@ from helpers.database import db_session
 class TestCreateRoom(BaseTestCase):
 
     def test_room_creation(self):
+        """
+        Testing for room creation
+        """
         execute_query = self.client.execute(
             room_mutation_query,
             context_value={'session': db_session})
