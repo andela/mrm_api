81887387dc02b7e33e54ee012c092e2ec970d0c7
Owen Byomuhangi <owen.byomuhangi@Owens-MacBook-Pro.local>
2018-04-29 20:04:14 +0300

    [Feature #156615347] Add tests

---
+++ tests/test_equipment/test_update_room_equipment.py
@@ -0,0 +1,33 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+
+from os.path import dirname, abspath
+mrm_api = dirname(dirname(abspath(__file__)))
+sys.path.insert(0, mrm_api)
+
+from tests.base import BaseTestCase
+from fixtures.equipment.update_resource_fixtures import (
+    update_room_resource_query,
+    expected_update_room_resource_query,
+    non_existant_room_id_query,
+    expected_non_existant_room_id_query,
+    non_existant_resource_id_query,
+    expected_non_existant_resource_id_query
+)
+
+class UpdateRoomResorce(BaseTestCase):
+    def test_update_resource(self):
+        query = self.client.execute(update_room_resource_query)
+        assert query == expected_update_room_resource_query
+
+    def test_nonexistant_room_id(self):
+        query = self.client.execute(non_existant_room_id_query)
+        assert query == expected_non_existant_room_id_query
+
+    def test_nonexistant_resource_id(self):
+        query = self.client.execute(non_existant_resource_id_query)
+        assert query == expected_non_existant_resource_id_query
+
+
+    
