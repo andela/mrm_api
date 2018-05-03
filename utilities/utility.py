f736c29d435c704e61cbb715a00037896940bc9c
Owen Byomuhangi <owen.byomuhangi@Owens-MacBook-Pro.local>
2018-05-03 12:16:15 +0300

    [Fix #156615347]Resolve conflicts

--- utilities/utility.py
+++ utilities/utility.py
@@ -8,11 +8,7 @@ def validate_empty_fields(**kwargs):
     """
     for field in kwargs:
         if not kwargs.get(field):
-<<<<<<< HEAD
             raise AttributeError(f"{field} is a required field")
-=======
-            raise AttributeError("Room {field} is required field")
->>>>>>> develop
 
 
 class Utility(object):
