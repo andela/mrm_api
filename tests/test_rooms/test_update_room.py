14897ab268fad9e24172c23736fda47d187a5d03
Owen Byomuhangi <owen.byomuhangi@Owens-MacBook-Pro.local>
2018-05-10 13:07:53 +0300

    [Fix #156614968]Resolve to use assertEquals
     To improve code readability , this commit changes the test mode of assertion from assert to assertEquals

--- tests/test_rooms/test_update_room.py
+++ tests/test_rooms/test_update_room.py
@@ -21,26 +21,26 @@ class TestUpdateRoom(BaseTestCase):
         Test if you can update all fields
         """
         test_update_all_fields = self.client.execute(query_update_all_fields)
-        assert test_update_all_fields == expected_query_update_all_fields
+        self.assertEquals(test_update_all_fields , expected_query_update_all_fields)
 
     def test_update_for_only_required_fields(self):
         """
         Test if you can edit only required field. We shall only require to update the name in this test.
         """ 
         test_edit_only_required_field = self.client.execute(query_update_only_required_field)
-        assert test_edit_only_required_field == expected_query_update_only_required_field
+        self.assertEquals(test_edit_only_required_field, expected_query_update_only_required_field) 
 
     def test_for_error_if_id_not_supplied(self):
         """
          Test if you get error once  'room_id' is not supplied in update query
         """
         test_get_error_if_no_id = self.client.execute(query_without_room_id)
-        assert test_get_error_if_no_id == expected_query_without_room_id
+        self.assertEquals(test_get_error_if_no_id, expected_query_without_room_id) 
 
     def test_for_error_if_room_id_is_non_existant_room(self):
         """
         Test if you get error once keyvalue 'room_id' supplied is of room that is non-existant
         """ 
         test_get_error_if_room_is_none_existant = self.client.execute(query_if_room_id_is_non_existant_room )
-        assert test_get_error_if_room_is_none_existant  == expected_query_if_room_id_is_non_existant_room 
+        self.assertEquals(test_get_error_if_room_is_none_existant, expected_query_if_room_id_is_non_existant_room)
 
