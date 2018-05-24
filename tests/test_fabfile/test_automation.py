e90f31dba0d1585c9d59807a9fd9dbb66d82d92d
Kamara Deo <kamaradeo@Kamara-Deo.local>
2018-05-24 09:45:59 +0300

    [Chore #156466913] Test fabfile

---
+++ tests/test_fabfile/test_automation.py
@@ -0,0 +1,22 @@
+import os
+from flask_testing import TestCase
+from fabric.api import *
+from ..fabfile import *
+
+
+class AutomationTestCase(TestCase):
+
+    def test_user(self):
+        current_user = local("users")
+        user = user_exists(current_user)
+        self.assertTrue(user)
+
+    def test_database_existence(self):
+        db = database_exists("mrm_test_db")
+        self.assertTrue(db)
+    
+    def test_check_dir(self):
+        count = check_dir()
+        self.assertIs(int,count)
+    
+
