354da166dc6c45dd38cf83f19d1973b64b0468d6
Owen Byomuhangi <owen.byomuhangi@Owens-MacBook-Pro.local>
2018-05-28 13:01:16 +0300

    [Feature #157385654]Add tests
    Add tests for the RoomSchedule query that contains google calendar intergration

--- tests/test_rooms/test_query_rooms.py
+++ tests/test_rooms/test_query_rooms.py
@@ -5,8 +5,9 @@ from fixtures.room.room_fixtures import (
     query_rooms_response,
     room_query_by_id,
     room_query_by_id_response,
-    room_query_by_nonexistant_id,
-    room_query_by_nonexistant_id_response
+    room_with_non_existant_id,
+    room_query_with_non_existant_id_response
+
 )
 
 
@@ -17,8 +18,8 @@ class QueryRooms(BaseTestCase):
 
     def test_query_room_with_id(self):
         query = self.client.execute(room_query_by_id)
-        self.assertEquals(query, room_query_by_id_response)
+        self.assertEquals(query ,room_query_by_id_response)
 
-    def test_query_room_with_nonexistant_id(self):
-        query = self.client.execute(room_query_by_nonexistant_id)
-        self.assertEquals(query, room_query_by_nonexistant_id_response)
+    def test_query_room_with_non_existant_id(self):
+        query = self.client.execute(room_with_non_existant_id)
+        self.assertEquals(query , room_query_with_non_existant_id_response)
