08e692d7b797acca55510b69b600f8b802bf6688
Kamara Deo <kamaradeo@Kamara-Deo.local>
2018-05-30 18:44:10 +0300

    [Chore #156466913] Fix flake8 errors

--- tests/test_fabfile/test_automation.py
+++ tests/test_fabfile/test_automation.py
@@ -24,3 +24,11 @@ class AutomationTestCase(BaseTestCase):
 
     def test_migration(self):
         self.assertTrue(run_migrations())
+    
+    def test_database_creation_and_existence(self):
+        current_user = local('users',capture=True)
+        test_db = create_database(owner=current_user,name='mrm_db_test')
+        db = database_exists("mrm_db_test")
+        self.assertTrue(db)
+        local('dropdb mrm_db_test')
+
