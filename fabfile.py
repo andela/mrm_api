e2d4489efd2e1e9d71cf95b2c344ade887cd41ef
Kamara Deo <kamaradeo@Kamara-Deo.local>
2018-05-30 11:47:54 +0300

    [Chore #156466913] Fix flake8 errors

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,5 @@
 from __future__ import with_statement
-from fabric.api import *
+from fabric.api import local, settings, hide
 import fnmatch
 import os
 
@@ -8,29 +8,32 @@ def user_exists(name):
     """
     Check if a user exists.
     """
-    with settings(hide('running', 'stdout','stderr', 'warnings'), warn_only=True):
+    with settings(hide('running', 'stdout', 'stderr', 'warnings'),
+                  warn_only=True):
         res = local('groups %(name)s' % locals()).succeeded
         return res
-    
+
+
 def create_database(owner, name, template='template0', encoding='UTF8',
                     locale='en_US.UTF-8'):
     """
     To create database
     """
-    with settings( warn_only=True):
+    with settings(warn_only=True):
         local('''createdb --owner %(owner)s --template %(template)s \
                     --encoding=%(encoding)s --lc-ctype=%(locale)s \
                     --lc-collate=%(locale)s %(name)s''' % locals())
-        
+
 
 def database_exists(name):
     """
     Check if a PostgreSQL database exists.
     """
-    with settings(hide('running','stdout', 'stderr', 'warnings'),
+    with settings(hide('running', 'stdout', 'stderr', 'warnings'),
                   warn_only=True):
         db = local('''psql -d %(name)s -c ""''' % locals()).succeeded
         return db
+
 
 def check_dir():
     """
@@ -39,27 +42,29 @@ def check_dir():
     files = os.listdir(path='alembic/versions/')
     number_migrations = len(fnmatch.filter(files, '*.py'))
     return number_migrations
-    
-def run_migrations():   
+
+
+def run_migrations():
     """
     To run migrations
     """
     with settings(hide('stdout', 'stderr', 'warnings'),
                   warn_only=True):
         res = check_dir()
-        if res > 0 and res <2:
+        if res > 0 and res < 2:
             local("alembic stamp head")
             local("alembic upgrade head")
-        if res==0:
+        if res == 0:
             local("alembic revision --autogenerate")
             local("alembic upgrade head")
-        if  res > 2:
+        if res > 2:
             local("alembic stamp head")
             local("alembic upgrade head")
             local("alembic revision --autogenerate")
             local("alembic upgrade head")
     return True
-          
+
+
 def run_app():
     """
     To start the app
@@ -67,6 +72,7 @@ def run_app():
     with settings(hide('stdout', 'stderr', 'warnings'),
                   warn_only=True):
         local("python manage.py runserver")
+
 
 def set_up(user):
     """
@@ -79,12 +85,9 @@ def set_up(user):
             run_migrations()
             run_app()
         else:
-             create_database(user,"mrm_db")
-             run_migrations()
-             run_app()
+            create_database(user, "mrm_db")
+            run_migrations()
+            run_app()
+        return ("Setup finished and App running")
     else:
-        print("System user doesnt exist")
-    
-
-
-    
+        return ("System user doesnt exist")
