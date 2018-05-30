9ba277374b9f353fe9960805d8ec777a2f276084
PatrickCmd <pwalukagga@gmail.com>
2018-05-30 11:57:05 +0300

    [Chore clean-develop] Remove double imports of os and sys

--- alembic/env.py
+++ alembic/env.py
@@ -3,9 +3,6 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 
-
-import os
-import sys
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
