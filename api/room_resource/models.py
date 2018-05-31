95bb1dbc8218a57c154553d7820339364a5107b3
PatrickCmd <pwalukagga@gmail.com>
2018-05-31 12:53:17 +0300

    [Feature 156871868] Add room resources
    [Feature 156871868] Remove un necessary imports

--- api/room_resource/models.py
+++ api/room_resource/models.py
@@ -6,7 +6,7 @@ from api.room.models import Room  # noqa: F401
 
 
 class Resource(Base, Utility):
-    __tablename__ = 'resource'
+    __tablename__ = 'resources'
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     room_id = Column(Integer, ForeignKey('rooms.id'))
