fe331b86719f753730a36fd589d1519c2a579121
Owen Byomuhangi <owen.byomuhangi@Owens-MacBook-Pro.local>
2018-06-06 09:39:14 +0300

    [Feature #157385654]Add tests for events

---
+++ tests/test_calendars/test_events.py
@@ -0,0 +1,25 @@
+"""This module deals with testing Calendar Integration with focus on
+    googleapi credentials
+"""
+from helpers.calendar.events import RoomSchedules
+from tests.base import BaseTestCase
+
+import sys
+import os
+sys.path.append(os.getcwd())
+
+
+class TestEvents(BaseTestCase):
+    """ This class tests for the google api credentials
+    func :
+        - test_response_events
+    """
+    def test_response_event(self):
+        """ This function tests for type of response
+        of the response to see if its a googleapi object
+        RoomSchedule function
+        """
+        calendarId = 'andela.com_3137313531373531393135@resource.calendar.google.com'  # noqa: E501
+        response = RoomSchedules.get_room_schedules(self, calendarId, 7)
+        assert type(response) is list
+        self.assertNotEquals(response, [])
