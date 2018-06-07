03dc3ca0263fef258cbbbeecff32f177ce96dd3d
Owen Byomuhangi <owen.byomuhangi@Owens-MacBook-Pro.local>
2018-06-07 14:30:18 +0300

    [Fix #157385654]Fix flake8 errors

--- fixtures/room/room_fixtures.py
+++ fixtures/room/room_fixtures.py
@@ -4,8 +4,8 @@ room_mutation_query = '''
     mutation {
         createRoom(
             name: "Mbarara", roomType: "Meeting", capacity: 4, floorId: 1,
-            imageUrl: "https://www.officelovin.com/wp-content/uploads/2016/10/andela-office-main-1.jpg",
-            calendarId:"andela.com_385@resource.calendar.google.com") {  
+            imageUrl: "https://www.officelovin.com/wp-content/uploads/2016/10/andela-office-main-1.jpg",  # noqa: E501
+            calendarId:"andela.com_385@resource.calendar.google.com") {
             room {
                 name
                 roomType
