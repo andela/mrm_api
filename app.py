7ab7f04118d4f624263460ccf579be049a118d6e
PatrickCmd <pwalukagga@gmail.com>
2018-06-08 15:05:30 +0300

    [Feature158212180] Enable cors

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_graphql import GraphQLView
 
 
@@ -9,8 +10,11 @@ from schema import schema
 
 def create_app(config_name):
     app = Flask(__name__)
+    CORS(app)
+
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+
     app.add_url_rule(
         '/mrm',
         view_func=GraphQLView.as_view(
