0da4dba8a431ce0a2600b04cf4ebb0a8119de7dc
Deo Kamara <deokamara@Deos-MacBook-Pro.local>
2018-06-11 19:14:16 +0300

    [Feature #158212394] healthcheck url setup

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_graphql import GraphQLView
 
 
 from config import config
 from helpers.database import db_session
 from schema import schema
+from healthcheck import HealthCheck
+import os
+
+
+health = HealthCheck()
+
+
+def postgres_up():
+    output = os.system("pg_isready")
+    return True, "postgress up ok 200"
+
+
+health = HealthCheck(checkers=[postgres_up])
 
 
 def create_app(config_name):
@@ -20,8 +33,9 @@ def create_app(config_name):
         )
     )
     app.add_url_rule(
-        '/healthcheck'
-        view_func=lambda:health.run()
+        '/healthcheck',
+        view_func=lambda: health.run(),
+        
     )
 
     @app.teardown_appcontext
