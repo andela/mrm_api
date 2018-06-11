cc787c6992b6879e3b812b112c998bbf6ec819a0
Joyce Namuli <joyce.namuli@andela.com>
2018-06-11 14:05:40 +0300

    Clean code with middleware

--- helpers/auth/decode_token.py
+++ helpers/auth/decode_token.py
@@ -11,20 +11,6 @@ class Authentication():
           user_credentials
     """
 
-    # def verify(self, token):
-    #     """ verify token
-    #       :params token
-    #       :return: func|string
-    #     """
-
-    #     if token:
-    #         return self.decode_token(token)
-    #     return jsonify(
-    #         {'message': 'This endpoint requires you to be authenticated.'}), 401
-
-
-    
-
     def decode_token(self, auth_token):
         """ Decodes the auth token
         :param auth_token:
@@ -33,7 +19,7 @@ class Authentication():
         SECRET_KEY = os.getenv('SECRET_KEY')
         try:
             payload = jwt.decode(auth_token, verify=False)
-            print(payload)
+            
             return payload
         except jwt.ExpiredSignatureError:
             return jsonify(
@@ -42,34 +28,5 @@ class Authentication():
         except jwt.InvalidTokenError:
             return jsonify(
                 {'message': 'Invalid token. Please Provide a valid token!'}), 401  # noqa E501
-
-    # def user_credentials(self, token):
-    #     value = self.verify(token)
-    #     if isinstance(value, dict):
-    #         user_email = value['UserInfo']['email']
-    #         nme = value['UserInfo']['name']
-    #         res = {'UserEmail': user_email, 'Name': nme}
-    #         return jsonify(res)
-    #     else:
-    #         return value
-
-    # def auth_required(self, fn):
-    #     """ Protects endpoint
-    #        :params: function
-    #        :return: function
-    #     """
-    #     def wrapper(*args, **kwargs):
-    #         """ Wrapper function
-    #           takes all the arguments that comes with the incoming function
-    #           makes sure that the end point has a token
-    #           :returns: func|string
-    #         """
-    #         token = request.headers.get('token')
-    #         validate_token = self.user_credentials(token)
-    #         if validate_token is True:
-    #             return fn(*args, **kwargs)
-    #         return validate_token
-    #     return wrapper
-
 
 Auth = Authentication()
--- helpers/middleware.py
+++ helpers/middleware.py
@@ -1,16 +1,17 @@
 from helpers.auth.decode_token import Authentication
-from flask import jsonify
+from flask import json
 
 
 class TokenMiddleware(object):
     def resolve(self,next, root, info, **args ):
         return_value = next(root, info, **args)
         token = info.context.headers.get('token')  # get the token from the headers.
-        user = Authentication.decode_token(self,auth_token=token) # decode the token from the headers
         if  token:
-            useremail = user['UserInfo']['email']
-            userename = user['UserInfo']['name']
-            return return_value
+            user = Authentication.decode_token(self,auth_token=token) # decode the token from the headers
+            user_details = user['UserInfo']
+            return user_details
+            
+            
         else:
             return return_value
         
