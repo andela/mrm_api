a5bacdd760a6d95635fcfac8d2de5536170cb8b8
Deo Kamara <deokamara@Deos-MacBook-Pro.local>
2018-06-25 16:17:38 +0300

    [Bug 158140829] Refactor tests

--- tests/test_rooms/test_update_room.py
+++ tests/test_rooms/test_update_room.py
@@ -4,16 +4,12 @@ from tests.base import BaseTestCase
 
 from fixtures.room.room_update_fixtures import (
     query_update_all_fields,
-<<<<<<< HEAD
     expected_query_update_all_fields,
     query_update_only_required_field,
     expected_query_update_only_required_field,
     query_without_room_id,
     expected_query_without_room_id,
     query_if_room_id_is_non_existant_room,
-=======
-    query_room_id_non_existant,
->>>>>>> [Bug 158140601] Restrict room info updates to admin users
     update_with_empty_field,
     query_without_room_id
 
@@ -26,6 +22,17 @@ from api.user_role.models import UsersRole
 
 class TestUpdateRoom(BaseTestCase):
 
+    def create_admin(self):
+        user = User(email="patrick.walukagga@andela.com",
+                    location="Kampala")
+        user.save()
+        role = Role(role="Admin")
+        role.save()
+        user_role = UsersRole(user_id=user.id, role_id=role.id)
+        user_role.save()
+        role = Role(role="Default User")
+        role.save()
+
     def test_resource_update_mutation_when_not_admin(self):
 
         api_headers = {'token': api_token}
@@ -35,45 +42,21 @@ class TestUpdateRoom(BaseTestCase):
                       str(response.data))
 
     def test_if_all_fields_updated(self):
-        user = User(email="patrick.walukagga@andela.com",
-                    location="Kampala")
-        user.save()
-        role = Role(role="Admin")
-        role.save()
-        user_role = UsersRole(user_id=user.id, role_id=role.id)
-        user_role.save()
-        role = Role(role="Default User")
-        role.save()
+        self.create_admin()
         api_headers = {'token': api_token}
         response = self.app_test.post('/mrm?query='+query_update_all_fields,
                                       headers=api_headers)
         self.assertIn("Jinja", str(response.data))
 
     def test_for_error_if_id_not_supplied(self):
-        user = User(email="patrick.walukagga@andela.com",
-                    location="Kampala")
-        user.save()
-        role = Role(role="Admin")
-        role.save()
-        user_role = UsersRole(user_id=user.id, role_id=role.id)
-        user_role.save()
-        role = Role(role="Default User")
-        role.save()
+        self.create_admin()
         api_headers = {'token': api_token}
         response = self.app_test.post('/mrm?query='+query_without_room_id,
                                       headers=api_headers)
         self.assertIn("required positional argument", str(response.data))
 
     def test_for_error_if_room_id_is_non_existant_room(self):
-        user = User(email="patrick.walukagga@andela.com",
-                    location="Kampala")
-        user.save()
-        role = Role(role="Admin")
-        role.save()
-        user_role = UsersRole(user_id=user.id, role_id=role.id)
-        user_role.save()
-        role = Role(role="Default User")
-        role.save()
+        self.create_admin()
         api_headers = {'token': api_token}
         response = self.app_test.post('/mrm?query='+query_room_id_non_existant,
                                       headers=api_headers)
