a1377bfb5564160f457cc95405e0230506bfff8a
Deo Kamara <deokamara@Deos-MacBook-Pro.local>
2018-06-26 09:05:02 +0300

    [Bug 158140829] Rebase with develop

--- fixtures/room_resource/room_resource_fixtures.py
+++ fixtures/room_resource/room_resource_fixtures.py
@@ -47,28 +47,7 @@ resource_mutation_quantity_string_query = '''
 '''
 
 
-<<<<<<< HEAD
-error_quantity_string = {
-  "errors": [
-    {
-      "message": "quantity is required field",
-      "locations": [
-        {
-          "column": 9,
-          "line": 3
-        }
-      ]
-    }
-  ],
-  "data": {
-    "createResource": None
-  }
-}
-
-resource_mutation_0_value_room_id_query = '''
-=======
 resource_mutation_0_room_id = '''
->>>>>>> [Bug 158140829] Fix flake8 errors
     mutation {
         createResource(
             name: "Speaker"
