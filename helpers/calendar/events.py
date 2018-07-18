f8d9e6c9291237ed2121c6d00fc6bfdf83d408fd
Mike Mutoro <strongestavenger@Mikes-MacBook-Pro.local>
2018-07-18 12:34:56 +0300

    [Chore 159002990] fix code smells

--- helpers/calendar/events.py
+++ helpers/calendar/events.py
@@ -10,7 +10,6 @@ class RoomSchedules(Credentials):
            create_room_event_schedules
            get_room_event_schedules
     """
-
     # define schedule methods here
     def get_room_schedules(self, calendar_id, days):
         """ Get room schedules. This method is responsible
@@ -19,28 +18,19 @@ class RoomSchedules(Credentials):
             - calendar_id
             - days(Time limit for the schedule you need)
         """
-
         service = Credentials.set_api_credentials(self)
         # 'Z' indicates UTC time
         now = datetime.datetime.utcnow().isoformat() + 'Z'
-
-        new_time = (
-            datetime.datetime.now() + datetime.timedelta(days=days)
-        ).isoformat() + 'Z'
-
+        new_time = (datetime.datetime.now() + datetime.timedelta(days=days)
+                    ).isoformat() + 'Z'
         events_result = service.events().list(
-            calendarId=calendar_id,
-            timeMin=now,
-            timeMax=new_time,
-            singleEvents=True,
-            orderBy='startTime').execute()
+            calendarId=calendar_id, timeMin=now, timeMax=new_time,
+            singleEvents=True, orderBy='startTime').execute()
 
         calendar_events = events_result.get('items', [])
         output = []
-
         if not calendar_events:
             return('No upcoming events found.')
-
         for event in calendar_events:
             event_details = {}
             event_details["start"] = event['start'].get('dateTime', event['start'].get('date'))  # noqa: E501
@@ -56,5 +46,4 @@ class RoomSchedules(Credentials):
                     r"(^[a-zA-Z0-9_.+-]+@andela+\.com+$)", attendees)
                 if match:
                     all_attendees.append(attendee.get('email'))
-
         return [all_attendees, output]
