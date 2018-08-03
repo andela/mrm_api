633b2327bf7430de83fb34b1b088284b508583b3
kensanni <kensanni@gmail.com>
2018-08-03 11:48:14 +0100

    chore(monitoring): Set up monitoring for application Install Bugsnag Add Bugsnag API key Integrate Bugsnag into application

--- manage.py
+++ manage.py
@@ -1,12 +1,21 @@
 import os
 
+import bugsnag
 from flask_script import Manager, Shell
+from bugsnag.flask import handle_exceptions
+
+# Configure bugnsag
+bugsnag.configure(
+  api_key=os.getenv('BUGSNAG_API_TOKEN'),
+  release_stage="development",
+  project_root="app"
+)
 
 # local imports
-from app import create_app
-
+from app import create_app  # noqa: E402
 
 app = create_app(os.getenv('APP_SETTINGS') or 'default')
+handle_exceptions(app)
 manager = Manager(app)
 
 
