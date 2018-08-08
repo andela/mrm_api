e95b7a9ca875e5b5c51e9ef26c257899180b3706
kensanni <kensanni@gmail.com>
2018-08-08 09:57:29 +0100

    chore(monitoring): Set up monitoring for application (#70)
    Install Bugsnag
    Add Bugsnag API key
    Integrate Bugsnag into application

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ from healthcheck_schema import healthcheck_schema
 def create_app(config_name):
     app = Flask(__name__)
     CORS(app)
-
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
