e99c392f29ce8de28b510997eedda91cc73c1ef1
lenny <lennykmutua@gmail.com>
2018-12-13 12:20:45 +0300

    chore(test_tokens):Delete all tokens from source code and add them to environment variables. (#162)
    - Delete all tokens from source code.
    
    - Add all tokens as environment variables.
    
    [Delivers #162545107]

--- tests/test_authentication/test_authentication.py
+++ tests/test_authentication/test_authentication.py
@@ -1,10 +1,11 @@
+import os
+import sys
+
 from tests.base import BaseTestCase
-from fixtures.token.token_fixture import invalid_token
 from fixtures.room.room_analytics_most_used_fixtures import (
     get_most_used_room_in_a_month_analytics_query)
+from fixtures.token.token_fixture import INVALID_TOKEN
 
-import sys
-import os
 sys.path.append(os.getcwd())
 
 
@@ -14,7 +15,7 @@ class TestAuthentication(BaseTestCase):
         Test that an error message is returned when a user
         has an invalid token
         """
-        headers = {"Authorization": "Bearer" + " " + invalid_token}
+        headers = {"Authorization": "Bearer" + " " + INVALID_TOKEN}  # noqa E501
         response = self.app_test.post(
             '/mrm?query=' + get_most_used_room_in_a_month_analytics_query,
             headers=headers)
