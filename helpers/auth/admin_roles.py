712c9e90310e8b84a3648adfa26564caa341ec59
RachaelNantale <Rachaelminani42@gmail.com>
2019-02-13 18:41:19 +0300

    bug(location): fix edit and delete location (#235)
    - add function to compare the admin's user location with current location
    - apply function to updateLocation and deleteLocation classes
    
    [Finishes #163741006]

--- helpers/auth/admin_roles.py
+++ helpers/auth/admin_roles.py
@@ -19,6 +19,14 @@ class Admin_roles():
         if admin_details.location != location.name:
             raise GraphQLError("You are not authorized to make changes in " + location.name)  # noqa: E501
 
+    def verify_admin_location(self, location_id):
+        admin_details = get_user_from_db()
+        location = Location.query.filter_by(id=location_id).first()
+        if admin_details.location != location.name:
+            raise GraphQLError(
+                "You are not authorized to make changes in " + location.name
+            )
+
     def update_delete_rooms_create_resource(self, room_id):
         admin_details = get_user_from_db()
         location_query = location_join_room()
@@ -39,12 +47,6 @@ class Admin_roles():
         floor_location = location_query.filter_by(id=floor_id).first()
         if admin_details.location != floor_location.name:
             raise GraphQLError("You are not authorized to make changes in " + floor_location.name)  # noqa: E501
-
-    def create_office(self, location_id):
-        admin_details = get_user_from_db()
-        location = Location.query.filter_by(id=location_id).first()
-        if location.name != admin_details.location:
-            raise GraphQLError("You are not authorized to make changes in " + location.name)  # noqa: E501
 
     def check_office_location_create_wing(self, floor_id):
         get_floor = FloorModel.query.filter_by(id=floor_id).first()
