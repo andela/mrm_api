861a2d6e07c04c0767d9fb360b91e949edb54f07
Philip Kwesiga <phil.kwesiga@gmail.com>
2019-03-14 15:18:10 +0300

    chore(docker) production image (#310)
    - change docker image variable name
    - run migrations on startup
    - allow supervisor to create logs file
    
    [Delivers #164626619]

--- docker/prod/start_gunicorn.sh
+++ docker/prod/start_gunicorn.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd /app
 export $(cat .env | xargs)
+# run migrations
+alembic upgrade head
+# run gunicorn
 gunicorn manage:app --worker-class gevent -b 0.0.0.0:8000 --reload --log-syslog
