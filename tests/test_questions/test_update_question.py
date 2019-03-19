9ef2bd112e50dad7005972ddf94fbbf2f75244e1
Runor Adjekpiyede <kampkellykeys@gmail.com>
2019-03-19 16:16:40 +0100

    chore(validate-question_types): validate question_types
    - Add validation for question types
    - Write tests
    
    [Finishes #164462447]

--- tests/test_questions/test_update_question.py
+++ tests/test_questions/test_update_question.py
@@ -5,7 +5,8 @@ from fixtures.questions.create_questions_fixtures import (
    update_question_mutation,
    update_question_response,
    update_question_invalidId,
-   query_update_total_views_of_questions
+   query_update_total_views_of_questions,
+   update_question_with_invalid_question_type
 )
 
 
@@ -33,6 +34,16 @@ class TestUpdateQuestion(BaseTestCase):
             self,
             update_question_invalidId,
             "Question not found"
+        )
+
+    def test_question_update_with_invalid_question_type(self):
+        """
+        Test question creation with field empty
+        """
+        CommonTestCases.admin_token_assert_in(
+            self,
+            update_question_with_invalid_question_type,
+            "Not a valid question type"
         )
 
     def test_increment_total_views_of_questions(self):
