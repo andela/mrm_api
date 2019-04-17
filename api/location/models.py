6909160eaea57f0e97c0aa20ded7eb83607243b3
Vincent Onyango <38049235+verenceLola@users.noreply.github.com>
2019-04-17 10:11:23 +0300

    feat(location name): add user location automatically upon user registration (#346) (#360)
    - update the location table to add the location of the user if it doesn't exist in the database.
    
    [Finishes #165124861]

--- api/location/models.py
+++ api/location/models.py
@@ -23,7 +23,7 @@ class Location(Base, Utility):
     __tablename__ = 'locations'
     id = Column(Integer, Sequence('floors_id_seq', start=1, increment=1), primary_key=True) # noqa
     name = Column(String, nullable=False)
-    abbreviation = Column(String, nullable=False)
+    abbreviation = Column(String)
     country = Column(Enum(CountryType))
     time_zone = Column(Enum(TimeZoneType))
     image_url = Column(String)
