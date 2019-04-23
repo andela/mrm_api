e4930128c2c2a0557ccdeffec603b53b3ad0b59f
Jesse Obinna Egbosionu <j3bsie@gmail.com>
2019-04-23 09:51:26 +0100

    feat(analytics): Implement get single room auto-cancellation analytics (#355)
    - Extend get_per_room function to return single room analytics
    - Add tests to cover feature
    
    [Finishes #164471450]

--- tests/test_events/test_events_ratios.py
+++ tests/test_events/test_events_ratios.py
@@ -8,6 +8,10 @@ from fixtures.events.events_ratios_fixtures import (
     event_ratio_for_one_day_query,
     event_ratio_per_room_query,
     event_ratio_per_room_response,
+    event_ratio_single_room_query,
+    event_ratio_single_room_response,
+    event_ratio_single_room_query_with_non_existing_id,
+    event_ratio_single_room_with_non_existing_id_response
 )
 
 sys.path.append(os.getcwd())
@@ -46,4 +50,26 @@ class TestEventRatios(BaseTestCase):
             self,
             event_ratio_per_room_query,
             event_ratio_per_room_response
+        )
+
+    def test_event_checkin_and_cancellation_single_room(self):
+        """
+        Test that an admin is able to get the ratio of checkins to bookings
+        for a single room
+        """
+        CommonTestCases.admin_token_assert_equal(
+            self,
+            event_ratio_single_room_query,
+            event_ratio_single_room_response
+        )
+
+    def test_event_checkin_cancellation_single_room_wrong_id(self):
+        """
+        Tests that an admin cannot get the ratio of check-ins to bookings for a
+        single room with invalid room id
+        """
+        CommonTestCases.admin_token_assert_equal(
+            self,
+            event_ratio_single_room_query_with_non_existing_id,
+            event_ratio_single_room_with_non_existing_id_response
         )
