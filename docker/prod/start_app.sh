f36715dac827ce59adf46371c3bcdc81bed563f8
Philip Kwesiga <phil.kwesiga@gmail.com>
2019-04-23 14:09:08 +0300

    chore(logging): run gunicorn with bash (#368)
    - Stop gunicorn from being run by supervisor
    - Run gunicorn with bash
    
    [Delivers #165116135]

--- docker/prod/start_app.sh
+++ docker/prod/start_app.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
+cd /app
+export $(cat .env | xargs)
+# run migrations
+alembic upgrade head
+# run gunicorn
+gunicorn --access-logfile '-' --workers 2 -t 3600 manage:app --worker-class gevent -b 0.0.0.0:8000 --reload  &
+
 function create_sock_file {
-  touch /var/run/supervisor.sock
-  chmod 777 /var/run/supervisor.sock
-  service supervisor start
-}
+      touch /var/run/supervisor.sock
+        chmod 777 /var/run/supervisor.sock
+          service supervisor start
+      }
 
-function run_application {
-  supervisorctl restart mrm_api
-}
-
-function run_celery {
-  supervisorctl restart celery
-}
+  function run_celery {
+        supervisorctl start celery
+    }
 
 function main {
-  create_sock_file
-  run_application
-}
+      create_sock_file
+        run_celery
+    }
 
 main $@
