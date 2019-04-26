ba22a7be1160cd015e895258c596d43c35c4b521
Timothy Kamau <teamorekamau97@gmail.com>
2019-04-26 12:26:09 +0300

    feat(clear-resource): clear a resource from a room when the resource is deleted from the resources table.
    - add delete resource functionality.
    - ensure that the resource is removed from all rooms it was assigned to.
    
    [Delivers #165594925]

--- tests/test_room_resource/test_delete_resource.py
+++ tests/test_room_resource/test_delete_resource.py
@@ -2,9 +2,8 @@ import sys
 import os
 
 from tests.base import BaseTestCase, CommonTestCases
-from fixtures.room_resource.delete_room_resource import (  # noqa: F401
-  delete_resource, expected_query_after_delete, delete_non_existant_resource)  # noqa: E501
-from helpers.database import db_session  # noqa: F401
+from fixtures.room_resource.delete_room_resource import (
+  delete_resource, expected_response_after_delete, delete_non_existent_resource)
 
 
 sys.path.append(os.getcwd())
@@ -12,7 +11,10 @@ sys.path.append(os.getcwd())
 
 class TestDeleteRoomResource(BaseTestCase):
 
-    def test_deleteresource_mutation_when_not_admin(self):
+    def test_delete_resource_mutation_when_not_admin(self):
+        """
+             Test user connot delete resource when not an admin
+        """
         CommonTestCases.user_token_assert_in(
             self,
             delete_resource,
@@ -20,15 +22,38 @@ class TestDeleteRoomResource(BaseTestCase):
         )
 
     def test_delete_resource_mutation_when_admin(self):
+        """
+            Test successful soft delete of a resource by an admin.
+        """
+        CommonTestCases.admin_token_assert_equal(
+            self,
+            delete_resource,
+            expected_response_after_delete
+        )
+
+    def test_delete_non_existent_resource(self):
+        """
+            Test that admin user cannot delete a resource that doesn't exist
+            in the database.
+        """
+        CommonTestCases.admin_token_assert_in(
+            self,
+            delete_non_existent_resource,
+            "Resource not found"
+        )
+
+    def test_delete_already_deleted_resource(self):
+        """
+            Test that admin user cannot delete a resource that has already been
+            soft deleted.
+        """
+        CommonTestCases.admin_token_assert_equal(
+            self,
+            delete_resource,
+            expected_response_after_delete
+        )
         CommonTestCases.admin_token_assert_in(
             self,
             delete_resource,
-            "Markers"
-        )
-
-    def test_non_existant_delete_resource_mutation(self):
-        CommonTestCases.admin_token_assert_in(
-            self,
-            delete_non_existant_resource,
             "Resource not found"
         )
