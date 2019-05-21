fc2b1a1ab5d48b74f4e4988d0621c0a3afd033af
Joshua Ocero <joshuaocero@yahoo.com>
2019-05-21 12:46:07 +0300

    166150047-bug(gunicorn): limit worker connections and timeout
    - Added worker connection limit of 1000
    - Added worker timeout of 30
    
    [Delivers 166150047]

--- docker/prod/start_gunicorn.sh
+++ docker/prod/start_gunicorn.sh
@@ -4,4 +4,4 @@ export $(cat .env | xargs)
 # run migrations
 alembic upgrade head
 # run gunicorn
-gunicorn manage:app --worker-class gevent -b 0.0.0.0:8000 --reload --log-syslog
+gunicorn manage:app --worker-class gevent --worker-connections 1000 --timeout 30 -b 0.0.0.0:8000 --reload --log-syslog
