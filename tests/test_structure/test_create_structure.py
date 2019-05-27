b005348c97f0c9a57465404d9cf245dcecfc061c
Jesse Obinna Egbosionu <j3bsie@gmail.com>
2019-05-27 14:32:52 +0100

    chore(model) - Make structure_id unique (#397)
    - Generate migration file
    - Update model definition
    - Extend validate_structure_id function to check for structure_id uniqueness
    
    [Finishes #165904360]

--- tests/test_structure/test_create_structure.py
+++ tests/test_structure/test_create_structure.py
@@ -1,6 +1,7 @@
 from tests.base import BaseTestCase, CommonTestCases
 from fixtures.structure.create_structure_fixtures import (
     office_structure_mutation_query,
+    office_structure_mutation_with_duplicates,
     office_structure_mutation_response,
 )
 
@@ -16,4 +17,32 @@ class TestCreateOfficeStructure(BaseTestCase):
             self,
             office_structure_mutation_query,
             office_structure_mutation_response
+        )
+
+    def test_structure_creation_payload_for_duplicates(self):
+        """
+        Tests that an admin cannot create an office structure with a
+        payload of duplicated structure ids
+        """
+
+        CommonTestCases.admin_token_assert_in(
+            self,
+            office_structure_mutation_with_duplicates,
+            'The office stuctures does not contain unique ids'
+        )
+
+    def test_structure_creation_for_duplicates(self):
+        """
+        Tests an admin cannot create an office structure twice
+        """
+        CommonTestCases.admin_token_assert_equal(
+            self,
+            office_structure_mutation_query,
+            office_structure_mutation_response
+        )
+
+        CommonTestCases.admin_token_assert_in(
+            self,
+            office_structure_mutation_query,
+            'Office 1 already exists'
         )
