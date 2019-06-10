00b7c436bfeabee1a1bec02f682e1b5402bd803e
David Kahara <kaharadave@gmail.com>
2019-06-10 16:46:32 +0300

    feat(update-office-structure): Implement updating of office structures (#416)
    - Write the logic for updating an office structure
    - Write tests for the new functionality
    
    [Finishes #165931398]

--- api/structure/schema.py
+++ api/structure/schema.py
@@ -5,9 +5,12 @@ from helpers.auth.authentication import Auth
 from helpers.structure.create_structure import create_structure
 from graphql import GraphQLError
 from helpers.auth.admin_roles import admin_roles
-from utilities.validations import validate_structure_id
 from api.room.schema import Room
 from api.room.models import Room as RoomModel
+from utilities.validations import (
+    validate_empty_fields,
+    validate_structure_id
+)
 from utilities.utility import update_entity_fields
 
 
@@ -92,11 +95,51 @@ class DeleteOfficeStructure(graphene.Mutation):
         return DeleteOfficeStructure(structure=structures)
 
 
+class UpdateOfficeStructure(graphene.Mutation):
+    """
+    Updates an office structure
+    """
+    class Arguments:
+        structure_id = graphene.String()
+        level = graphene.Int()
+        name = graphene.String()
+        parent_id = graphene.String()
+        parent_title = graphene.String()
+        tag = graphene.String()
+        location_id = graphene.Int()
+        position = graphene.Int()
+    structure = graphene.Field(Structure)
+
+    @Auth.user_roles('Admin')
+    def mutate(self, info, structure_id, **kwargs):
+        validate_empty_fields(**kwargs)
+        query = Structure.get_query(info)
+        active_structure = query.filter(
+            StructureModel.structure_id == structure_id,
+            StructureModel.state == "active").first()
+        if not active_structure:
+            raise GraphQLError('Structure not found')
+        update_entity_fields(active_structure, **kwargs)
+        active_structure.save()
+        return UpdateOfficeStructure(structure=active_structure)
+
+
 class Mutation(graphene.ObjectType):
     create_office_structure = CreateOfficeStructure.Field()
     delete_office_structure = DeleteOfficeStructure.Field(
         description="Deletes office structures and the rooms associated\
         \n- structure_ids: The structure_id of the structure(s)"
+    )
+    update_office_structure = UpdateOfficeStructure.Field(
+        description="Updates an office structure and takes the arguments\
+            \n- structure_id: The structure id for the office structure\
+            \n- level: The level of the office structure\
+            \n- name: The name of the office structure\
+            \n- parent_id: The parent id of the office structure\
+            \n- parent_title: The parent title of the structure\
+            \n- tag: Tags for the office structure\
+            \n- location_id: The location id of the office structure\
+            \n- position: The position of the office structure"
     )
 
 
