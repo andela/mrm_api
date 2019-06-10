3c073974ece2e4e958b4f05448022e49186e8c53
Kelvin Mwangi <33450849+kwanj-k@users.noreply.github.com>
2019-06-10 21:02:06 +0300

    feat(query-device): enable admin to query device by name (#421)
    - Enable admin to query devices by name
    - Add relevant tests
    
    [Finishes #166285928]

--- tests/test_devices/test_specific_device.py
+++ tests/test_devices/test_specific_device.py
@@ -5,10 +5,12 @@ from fixtures.devices.devices_fixtures import (
     query_non_existent_device,
     expected_error_non_existent_device_id
 )
-
-import sys
-import os
-sys.path.append(os.getcwd())
+from fixtures.devices.devices_fixtures import (
+    search_device_by_name,
+    search_non_existing_device,
+    search_device_by_name_expected_response,
+    search_non_existing_device_response
+)
 
 
 class TestGetSpecificDevice(BaseTestCase):
@@ -28,4 +30,18 @@ class TestGetSpecificDevice(BaseTestCase):
             self,
             query_non_existent_device,
             expected_error_non_existent_device_id
+        )
+
+    def test_search_device_by_name(self):
+        CommonTestCases.admin_token_assert_equal(
+            self,
+            search_device_by_name,
+            search_device_by_name_expected_response
+        )
+
+    def test_search_non_existing_device(self):
+        CommonTestCases.admin_token_assert_equal(
+            self,
+            search_non_existing_device,
+            search_non_existing_device_response
         )
