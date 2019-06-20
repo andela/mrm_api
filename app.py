e5de19ada1fd9d780e8914d297381bcc543cb7cd
[Arusey] <[kevinkibetw7@gmail.com]>
2019-06-20 12:53:22 +0300

    CON-72-story(admin-notifications): setup admin notifications - setup folder structure for implementing notifications structure - implement notification when admin has not been seen in a while
    [Delivers CON-72]

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template
 from flask_graphql import GraphQLView
 from flask_cors import CORS
 from flask_json import FlaskJSON
+from flask_socketio import SocketIO
 
 from flask_mail import Mail
 from config import config
@@ -19,6 +20,7 @@ def create_app(config_name):
     app = Flask(__name__)
     CORS(app)
     FlaskJSON(app)
+    SocketIO(app)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
     mail.init_app(app)
