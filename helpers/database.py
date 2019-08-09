77984d49f6d43f93cc630323cd018b07c02a4f12
Daniel Kamar <kamarster@gmail.com>
2019-08-09 11:57:13 +0300

    CON-212-story(timestamps): add timestamps fields to all database records
    - add created_at and updated_at fields to all database records
    
    [Delivers CON-212]

--- helpers/database.py
+++ helpers/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, DateTime, Column, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 from config import config
@@ -14,5 +14,16 @@ db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
 
-Base = declarative_base()
-Base.query = db_session.query_property()
+DeclarativeBase = declarative_base()
+DeclarativeBase.query = db_session.query_property()
+
+
+class Base(DeclarativeBase):
+    """
+    Extends the base class to the specific models and by default places the
+    date_created and date_updated fields on all the tables in the database.
+    """
+    __abstract__ = True  # directive for class should not be mapped to a table
+    date_created = Column(DateTime, server_default=func.now())
+    date_updated = Column(DateTime, server_default=func.now(),
+                          onupdate=func.now())
