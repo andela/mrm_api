a51c4acb191253948d15d92b893efa1131097d0e
Nelson Mwirumubi <mwinel@live.com>
2019-08-25 10:32:28 +0300

    --amend

--- tests/test_devices/test_delete_device.py
+++ tests/test_devices/test_delete_device.py
@@ -1,8 +1,8 @@
 from tests.base import BaseTestCase, CommonTestCases
 from fixtures.devices.devices_fixtures import (
     delete_device_mutation,
-    delete_device_response
-)
+    delete_non_exiting_device_mutation,
+    delete_device_response)
 
 
 class TestDeleteDevices(BaseTestCase):
@@ -11,4 +11,11 @@ class TestDeleteDevices(BaseTestCase):
             self,
             delete_device_mutation,
             delete_device_response
+        )
+
+    def test_delete_non_existing_device(self):
+        CommonTestCases.admin_token_assert_in_errors(
+            self,
+            delete_non_exiting_device_mutation,
+            "Device not found"
         )
