14bf28cc44f8f8b07847286e462384b737e92780
Emmanuel Raji <rojo.mosco@gmail.com>
2019-09-13 15:44:17 +0100

    CON-326-fix(remote-rooms): include Kigali rooms
    - ensure remote rooms are showing uo in kigali location
    
    [Fixes CON-326]

--- helpers/remote_rooms/remote_rooms_location.py
+++ helpers/remote_rooms/remote_rooms_location.py
@@ -8,5 +8,6 @@ def map_remote_room_location_to_filter():
             'Lagos': lambda remote_room: remote_room.find('ET-') != -1,
             'Kampala': lambda remote_room: remote_room.find('Kampala') != -1
             and not remote_room.startswith('Nairobi'),
+            'Kigali': lambda remote_room: remote_room.find('Kigali') != -1,
             'all': lambda remote_room: True  # return True for all rooms
         }
